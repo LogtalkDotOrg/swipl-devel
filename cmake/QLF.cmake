0c7f397ae54395cac008befaa214b5b5f2c428bc
Jan Wielemaker <J.Wielemaker@cs.vu.nl>
2018-09-17 11:58:34 +0200

    Final installation of QLF files

--- cmake/QLF.cmake
+++ cmake/QLF.cmake
@@ -1,13 +1,31 @@
 # compile_qlf spec ...
 
+# run_installed_swipl(command [QUIET] [SCRIPT script ...] [COMMENT comment])
+# Run Prolog with its home set to the installed system.  This is used for
+# post installation tasks
+
 function(run_installed_swipl command)
-  install(CODE "EXECUTE_PROCESS(COMMAND swipl -f none --home=${SWIPL_INSTALL_PREFIX} -g \"${command}\" -t halt)")
+  set(options -f none -t halt --home=${SWIPL_INSTALL_PREFIX})
+  cmake_parse_arguments(my "QUIET" "COMMENT" "SCRIPT" ${ARGN})
+  if(my_QUIET)
+    set(options ${options} -q)
+  endif()
+  if(my_COMMENT)
+    install(CODE "message(\"${my_COMMENT}\")")
+  endif()
+  foreach(s ${my_SCRIPT})
+    set(options ${options} -s ${s})
+  endforeach()
+  install(CODE "EXECUTE_PROCESS(COMMAND swipl ${options} -g \"${command}\")")
 endfunction()
+
+# qcompile(spec ...)
+# Generate QLF files for the given libraries.
 
 function(qcompile)
   foreach(f ${ARGN})
-    install(CODE "message(\"-- QLF compiling ${f}\")")
-    run_installed_swipl("qcompile(${f})")
+    run_installed_swipl("qcompile(${f})"
+			COMMENT "-- QLF compiling ${f}")
   endforeach()
 endfunction()
 
